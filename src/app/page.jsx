23a0b263e4e8507c52f367f172a5b177efbc165d
Facundo Nadaf <facundo.nadaf@integrify.io>
2023-01-25 20:14:21 +0100

    added img and changed styles

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,20 @@
 // import './globals.css';
+import Image from 'next/image';
+import homePicture from './images/home.jpg';
 
 export default function Home() {
 	return (
 		<main className="bg-gray-400 h-screen pt-[50px]">
-			<h1>Pagina principal de nutricion</h1>
+			<div>
+				<Image
+					src={homePicture}
+					alt="imagen de comida saludable"
+					className="w-full h-fit"
+				/>
+			</div>
+			<div>
+				<h1>Pagina principal de nutricion</h1>
+			</div>
 		</main>
 	);
 }
